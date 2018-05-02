1f6b926d1bda73ac4b6c12a8aa79b198b6f804df
Bradley Moore <bradleym007@gmail.com>
2018-05-02 13:16:19 -0700

    Made uuid function public, along with a much more elegant random uuid

--- wp-stripe-api.php
+++ wp-stripe-api.php
@@ -113,22 +113,9 @@ if ( ! class_exists( 'StripeAPI' ) ) {
 			return $this->build_request( $route, $body, $method )->fetch();
 		}
 
-		protected function new_uuid(){
-			return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-				// 32 bits for "time_low"
-				mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
-				// 16 bits for "time_mid"
-				mt_rand( 0, 0xffff ),
-				// 16 bits for "time_hi_and_version",
-				// four most significant bits holds version number 4
-				mt_rand( 0, 0x0fff ) | 0x4000,
-				// 16 bits, 8 bits for "clk_seq_hi_res",
-				// 8 bits for "clk_seq_low",
-				// two most significant bits holds zero and one for variant DCE1.1
-				mt_rand( 0, 0x3fff ) | 0x8000,
-				// 48 bits for "node"
-				mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
-			);
+		public function new_uuid(){
+			// Can we just bin2hex(random_bytes(16)) instead? For cleanliness.
+			return bin2hex( random_bytes( 16 ) );
 		}
 
 		/* ------------------- CORE RESOURCES --------------------- */
